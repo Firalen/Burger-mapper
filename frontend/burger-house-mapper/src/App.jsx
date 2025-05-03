6385d154cde1bfca11a0dec6348f6da94a599cff
fira <fira81564@gmail.com>
2025-05-03 16:03:36 +0300

    new

--- frontend/burger-house-mapper/src/App.jsx
+++ frontend/burger-house-mapper/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   return (
     <>
-     home
+     <h1 className="text-red-800 font-semibold text-3xl text-center p-10">ADAMA DATE PLACES</h1>
+     <p>"Dating Place" is your personal guide to the most romantic spots around town.
+      Whether you're planning your first date or a special anniversary, find cozy cafés,
+     scenic walks, rooftop dinners, and more – all in one place. Book your perfect date location with ease!"</p>
     </>
   )
 }
