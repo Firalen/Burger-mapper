b700bb08e40fad282c1ec1e38cb20e087d6ce248
fira <fira81564@gmail.com>
2025-05-05 11:39:59 +0300

    updated

--- frontend/burger-house-mapper/src/App.jsx
+++ frontend/burger-house-mapper/src/App.jsx
@@ -1,4 +1,4 @@
-
+import image1 from './assets/haileresort.jpg';
 
 
 function App() {
@@ -6,10 +6,18 @@ function App() {
 
   return (
     <>
-     <h1 className="text-red-800 font-semibold text-3xl text-center p-10">ADAMA DATE PLACES</h1>
-     <p>"Dating Place" is your personal guide to the most romantic spots around town.
-      Whether you're planning your first date or a special anniversary, find cozy cafés,
-     scenic walks, rooftop dinners, and more – all in one place. Book your perfect date location with ease!"</p>
+     
+     <div className='flex'>
+     <div className=" text-2xl text-center p-10 font-light">
+     <h1 className="text-red-800 font-italic text-3xl text-center p-10">ADAMA DATE PLACES</h1>
+      <p>"Dating Place" is your personal guide to the most romantic spots around town.</p>
+      <p>  Whether you're planning your first date or a special anniversary, find cozy cafés,</p>
+      <p> scenic walks, rooftop dinners, and more – all in one place. Book your perfect date </p>location with ease!"
+     </div>
+        <div className='w-1/2 p-10'>
+          <img src={image1} alt="Haileresort" className="w-full h-auto object-cover" />
+        </div>
+     </div>
     </>
   )
 }
