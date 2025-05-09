e1324ee684b12008a3577ba99b8c537cfba133e4
fira <fira81564@gmail.com>
2025-05-09 23:24:14 +0300

    updated

--- frontend/burger-house-mapper/src/home.jsx
+++ frontend/burger-house-mapper/src/home.jsx
@@ -3,29 +3,27 @@ import About from './about';
 import PlacesList from './places';
 import Review from './review';
 import Footer from './footer';
-function Home() {
- 
 
+function Home() {
   return (
     <div>
-     
-     <div className='flex flex-col md:flex-row items-center justify-center bg-rose-50 pt-5'>
-     <div className=" text-2xl text-center p-10 ">
-     <h1 className="text-black font-italic text-3xl text-center p-10">ADAMA DATE PLACES</h1>
-      <p>"Dating Place" is your personal guide to the most romantic spots around town.</p>
-      <p>  Whether you're planning your first date or a special anniversary, find cozy cafés,</p>
-      <p> scenic walks, rooftop dinners, and more – all in one place. Book your perfect date </p>location with ease!"
-     </div>
-        <div className='w-1/2 p-10'>
+      <div className="flex flex-col md:flex-row items-center justify-center bg-rose-50 pt-5">
+        <div className="text-2xl text-center p-10">
+          <h1 className="text-black font-italic text-3xl text-center p-10">ADAMA DATE PLACES</h1>
+          <p>"Dating Place" is your personal guide to the most romantic spots around town.</p>
+          <p>Whether you're planning your first date or a special anniversary, find cozy cafés,</p>
+          <p>scenic walks, rooftop dinners, and more – all in one place. Book your perfect date location with ease!</p>
+        </div>
+        <div className="w-1/2 p-10">
           <img src={image1} alt="Haileresort" className="w-full h-auto object-cover" />
         </div>
-     </div>
-     <About />
-     <PlacesList />
-     <Review />
-     <Footer />
+      </div>
+      <About />
+      <PlacesList />
+      <Review />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
