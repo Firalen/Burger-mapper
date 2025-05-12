9c9f09698cc2a5a9358ba58c8dd0ca0b1b24b089
fira <fira81564@gmail.com>
2025-05-12 23:32:29 +0300

    commit

--- frontend/burger-house-mapper/src/nav.jsx
+++ frontend/burger-house-mapper/src/nav.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-function nav() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Nav() {
   return (
     <div>
       <nav className="bg-gray-800 p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-white text-2xl font-bold">ADAMA DATE PLACES</h1>
-          <Link to="/" className="text-white hover:text-gray-300 font-bold">HOME</Link>
-          <Link to="/review" className="text-white hover:text-gray-300 font-bold">ABOUT</Link> 
-          <Link to="/contact" className="text-white hover:text-gray-300 font-bold">CONTACT</Link>
-          <Link to="/places" className="text-white hover:text-gray-300 font-bold">PLACES</Link>
+          {/* Logo */}
+          <h1 className="text-white text-2xl font-bold">
+            <Link to="/" className="hover:text-gray-300">
+              ADAMA DATE PLACES
+            </Link>
+          </h1>
+
+          {/* Navigation Links */}
+          <div className="flex space-x-6">
+            <Link to="/" className="text-white hover:text-gray-300 font-bold">
+              HOME
+            </Link>
+            <Link to="/about" className="text-white hover:text-gray-300 font-bold">
+              ABOUT
+            </Link>
+            <Link to="/contact" className="text-white hover:text-gray-300 font-bold">
+              CONTACT
+            </Link>
+            <Link to="/places" className="text-white hover:text-gray-300 font-bold">
+              PLACES
+            </Link>
+            <Link to="/events" className="text-white hover:text-gray-300 font-bold">
+              EVENTS
+            </Link>
+          </div>
         </div>
-        </nav>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default nav
+export default Nav;
